1a70cb0bd078e389ec959a4c7af69c708c4c0613
Alberto Puliga <albi.puliga@gmail.com>
2024-10-12 13:54:15 +0200

    test: updated tests

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -1,69 +1,87 @@
 from flask import request
+
 from iebank_api import app, db
 from iebank_api.models import Account
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/skull', methods=['GET'])
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/skull", methods=["GET"])
 def skull():
-    text = 'Hi! This is the BACKEND SKULL! 💀 '
-    
-    text = text +'<br/>Database URL:' + db.engine.url.database
+    text = "Hi! This is the BACKEND SKULL! 💀 "
+
+    if db.engine.url.database:
+        text = text + "<br/>Database URL:" + db.engine.url.database
     if db.engine.url.host:
-        text = text +'<br/>Database host:' + db.engine.url.host
+        text = text + "<br/>Database host:" + db.engine.url.host
     if db.engine.url.port:
-        text = text +'<br/>Database port:' + db.engine.url.port
+        text = text + "<br/>Database port:" + str(db.engine.url.port)
     if db.engine.url.username:
-        text = text +'<br/>Database user:' + db.engine.url.username
+        text = text + "<br/>Database user:" + db.engine.url.username
     if db.engine.url.password:
-        text = text +'<br/>Database password:' + db.engine.url.password
+        text = text + "<br/>Database password:" + db.engine.url.password
     return text
 
-@app.route('/accounts', methods=['POST'])
+
+@app.route("/accounts", methods=["POST"])
 def create_account():
-    name = request.json['name']
-    currency = request.json['currency']
-    country = request.json['country']
+    if not request.json:
+        return {"error": "Invalid input"}, 400
+
+    name = request.json.get("name")
+    currency = request.json.get("currency")
+    country = request.json.get("country")
     account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
 
-@app.route('/accounts', methods=['GET'])
+
+@app.route("/accounts", methods=["GET"])
 def get_accounts():
     accounts = Account.query.all()
-    return {'accounts': [format_account(account) for account in accounts]}
+    return {"accounts": [format_account(account) for account in accounts]}
 
-@app.route('/accounts/<int:id>', methods=['GET'])
+
+@app.route("/accounts/<int:id>", methods=["GET"])
 def get_account(id):
     account = Account.query.get(id)
     return format_account(account)
 
-@app.route('/accounts/<int:id>', methods=['PUT'])
+
+@app.route("/accounts/<int:id>", methods=["PUT"])
 def update_account(id):
     account = Account.query.get(id)
-    account.name = request.json['name']
-    account.country = request.json['country']
+    if not request.json:
+        return {"error": "Invalid input"}, 400
+    if account is None:
+        return {"error": "Account not found"}, 404
+
+    account.name = request.json.get("name")
+    account.country = request.json["country"]
     db.session.commit()
     return format_account(account)
 
-@app.route('/accounts/<int:id>', methods=['DELETE'])
+
+@app.route("/accounts/<int:id>", methods=["DELETE"])
 def delete_account(id):
     account = Account.query.get(id)
     db.session.delete(account)
     db.session.commit()
     return format_account(account)
 
+
 def format_account(account):
     return {
-        'id': account.id,
-        'name': account.name,
-        'account_number': account.account_number,
-        'balance': account.balance,
-        'currency': account.currency,
-        'country': account.country,
-        'status': account.status,
-        'created_at': account.created_at
+        "id": account.id,
+        "name": account.name,
+        "account_number": account.account_number,
+        "balance": account.balance,
+        "currency": account.currency,
+        "country": account.country,
+        "status": account.status,
+        "created_at": account.created_at,
     }
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,5 +1,5 @@
 from iebank_api.models import Account
-import pytest
+
 
 def test_create_account():
     """
@@ -7,9 +7,27 @@ def test_create_account():
     WHEN a new Account is created
     THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
     """
-    account = Account('John Doe', '€')
-    assert account.name == 'John Doe'
-    assert account.currency == '€'
-    assert account.account_number != None
+    account = Account("John Doe", "€", "Italy")
+    assert account.name == "John Doe"
+    assert account.currency == "€"
+    assert account.account_number is not None
     assert account.balance == 0.0
-    assert account.status == 'Active'
+    assert account.status == "Active"
+    assert account.country == "Italy"
+    
+def test_edit_account():
+    """
+    GIVEN a Account model
+    WHEN a new Account is created and edited
+    THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
+    """
+    account = Account("John Doe", "€", "Italy")
+    account.name = "Jane Doe"
+    account.currency = "$"
+    account.country = "USA"
+    assert account.name == "Jane Doe"
+    assert account.currency == "$"
+    assert account.country == "USA"
+    assert account.account_number is not None
+    assert account.balance == 0.0
+    assert account.status == "Active"

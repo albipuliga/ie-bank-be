cdf5fa976d358b4ec177d3eddf4ff996bfb1fc38
Alberto Puliga <albi.puliga@gmail.com>
2024-10-12 19:14:31 +0200

    feat: added uat config

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config(object):
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = "this-really-needs-to-be-changed"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class LocalConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///local.db"
     # SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     # dbuser=os.getenv('DBUSER'),
     # dbpass=os.getenv('DBPASS'),
@@ -17,15 +20,26 @@ class LocalConfig(Config):
     # )
     # DEBUG = True
 
+
 class GithubCIConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
     DEBUG = True
 
+
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
+    SQLALCHEMY_DATABASE_URI = "postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
+        dbuser=os.getenv("DBUSER"),
+        dbpass=os.getenv("DBPASS"),
+        dbhost=os.getenv("DBHOST"),
+        dbname=os.getenv("DBNAME"),
+    )
+    DEBUG = True
+
+class UatConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
+        dbuser=os.getenv("DBUSER"),
+        dbpass=os.getenv("DBPASS"),
+        dbhost=os.getenv("DBHOST"),
+        dbname=os.getenv("DBNAME"),
     )
     DEBUG = True
